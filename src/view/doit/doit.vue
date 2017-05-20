3e0f21aa732b5014116e39145eee7ed58079793f
苏之瑜 <suzhiyu@yonyou.com>
2017-05-20 10:37:51 +0800

    laopei

---
+++ src/view/doit/doit.vue
@@ -0,0 +1,21 @@
+<template>
+老裴，好久不见！
+</template>
+<script type="text/javascript">
+	export default{
+		data(){
+			return{
+				
+			}
+		},
+		methods:{
+
+		},
+		mounted(){
+			console.log(this.$route.params)
+		}
+	}
+</script>
+<style type="text/css">
+
+</style>
